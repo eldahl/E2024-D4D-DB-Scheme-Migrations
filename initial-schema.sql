7293213ffb7c21db743b5a8cfd07d29cb6fffdd8
AsbjrnJacobsen <megetunikemail@gmail.com>
2024-09-20 16:32:28 +0200

    Added Auto Increment
    Had to add auto increment, because it was not automatic.

--- initial-schema.sql
+++ initial-schema.sql
@@ -14,7 +14,7 @@ GO
 
 create table Activity
 (
-	id int not null,
+	id int not null IDENTITY(1, 1),
 	nameActivity varchar(25) not null,
 	activityDescription varchar(200) not null,
 	constraint PK_Activity Primary key (id)
@@ -23,7 +23,7 @@ create table Activity
 
 create table ActivityDuration
 (
-	id int not null,
+	id int not null IDENTITY(1, 1),
 	activityId int not null,
 	startTime datetime not null,
 	durationTime time,
