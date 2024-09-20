acdb9b1a02d892fea71e9130308748c84e4a5a05
AsbjrnJacobsen <megetunikemail@gmail.com>
2024-09-20 15:12:29 +0200

    Create initial-schema.sql

---
+++ initial-schema.sql
@@ -0,0 +1,37 @@
+use master
+go
+
+IF DB_ID('ActivityDB') IS NOT NULL    
+BEGIN
+	DROP DATABASE ActivityDB			
+END
+
+CREATE DATABASE ActivityDB
+GO
+
+USE ActivityDB	
+GO
+
+create table Activity
+(
+	id int not null,
+	nameActivity varchar(25) not null,
+	activityDescription varchar(200) not null,
+	constraint PK_Activity Primary key (id)
+	
+)
+
+create table ActivityDuration
+(
+	id int not null,
+	activityId int not null,
+	startTime datetime not null,
+	durationTime time,
+	constraint PK_ActivityDuration primary key (id)
+)
+
+create table ActivityCategory
+(
+	catName varchar(100) not null,
+	constraint PK_ActivityCategory primary key (catName)
+)
