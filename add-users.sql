971307d4d7aab6e4d5169183e01cdbc595653881
AsbjrnJacobsen <megetunikemail@gmail.com>
2024-09-20 16:35:01 +0200

    Added auto increment
    also had to add auto increment here

--- add-users.sql
+++ add-users.sql
@@ -3,7 +3,7 @@ go
 
 create table Users
 (
-	id int not null,
+	id int not null IDENTITY(1, 1),
 	userName varchar(50) not null,
 	email varchar(100) not null,
 	constraint PK_Users primary key (id)
