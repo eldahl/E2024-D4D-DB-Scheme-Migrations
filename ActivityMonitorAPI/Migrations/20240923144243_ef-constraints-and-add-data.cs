400f9bc16ebc741fb7fda2a61c175d0be6408112
eldahl <71466904+eldahl@users.noreply.github.com>
2024-09-23 16:47:03 +0200

    Migration 3 - constriants and add data Co-authored-by: AsbjrnJacobsen <megetunikemail@gmail.com>

---
+++ ActivityMonitorAPI/Migrations/20240923144243_ef-constraints-and-add-data.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ActivityMonitorAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class efconstraintsandadddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "categoryName",
+                table: "Activities",
+                type: "varchar(100)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "ActivityCategory",
+                column: "catName",
+                values: new object[]
+                {
+                    "Afk",
+                    "Gaming",
+                    "Leisure",
+                    "Meeting",
+                    "Research",
+                    "Work"
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "id", "email", "userName" },
+                values: new object[,]
+                {
+                    { 1, "larsGmanHenning@gmail.com", "Lars Henning" },
+                    { 2, "Paludan@gmail.com", "Peter Paludan" },
+                    { 3, "henribobendi@gmail.com", "Henriette" },
+                    { 4, "JensJensJens@gmail.com", "Jens Jensen" },
+                    { 5, "belastende@gmail.com", "Bent Belastende" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Activities",
+                columns: new[] { "id", "activityDescription", "categoryName", "nameActivity" },
+                values: new object[,]
+                {
+                    { 1, "Actively using IDE", "Work", "Coding" },
+                    { 2, "Relaxing on youtube", "Leisure", "Watching youtube" },
+                    { 3, "Acquiring new knowledge", "Research", "Stackoverflow" },
+                    { 4, "PC is idle", "Afk", "No user activity" },
+                    { 5, "Planning..? Shittalking..? Its on discord at least.", "Meeting", "Discord Meeting" },
+                    { 6, "Limiting EXP waste", "Gaming", "Playing OSRS" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ActivityCategories",
+                columns: new[] { "id", "activityId", "durationTime", "startTime" },
+                values: new object[,]
+                {
+                    { 1, 1, new TimeSpan(0, 2, 0, 0, 0), new DateTime(2024, 9, 23, 16, 42, 43, 476, DateTimeKind.Local).AddTicks(3843) },
+                    { 2, 2, new TimeSpan(0, 2, 0, 0, 0), new DateTime(2024, 9, 23, 16, 42, 43, 476, DateTimeKind.Local).AddTicks(3913) },
+                    { 3, 3, new TimeSpan(0, 2, 0, 0, 0), new DateTime(2024, 9, 23, 16, 42, 43, 476, DateTimeKind.Local).AddTicks(3915) },
+                    { 4, 4, new TimeSpan(0, 2, 0, 0, 0), new DateTime(2024, 9, 23, 16, 42, 43, 476, DateTimeKind.Local).AddTicks(3916) },
+                    { 5, 5, new TimeSpan(0, 2, 0, 0, 0), new DateTime(2024, 9, 23, 16, 42, 43, 476, DateTimeKind.Local).AddTicks(3918) },
+                    { 6, 6, new TimeSpan(0, 2, 0, 0, 0), new DateTime(2024, 9, 23, 16, 42, 43, 476, DateTimeKind.Local).AddTicks(3919) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityCategories_activityId",
+                table: "ActivityCategories",
+                column: "activityId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_categoryName",
+                table: "Activities",
+                column: "categoryName",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_activityCategory",
+                table: "Activities",
+                column: "categoryName",
+                principalTable: "ActivityCategory",
+                principalColumn: "catName",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ActivityDuration_Activity",
+                table: "ActivityCategories",
+                column: "activityId",
+                principalTable: "Activities",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_activityCategory",
+                table: "Activities");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ActivityDuration_Activity",
+                table: "ActivityCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ActivityCategories_activityId",
+                table: "ActivityCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Activities_categoryName",
+                table: "Activities");
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategories",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategories",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategories",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategories",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategories",
+                keyColumn: "id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategories",
+                keyColumn: "id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Activities",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Activities",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Activities",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Activities",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Activities",
+                keyColumn: "id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Activities",
+                keyColumn: "id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategory",
+                keyColumn: "catName",
+                keyValue: "Afk");
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategory",
+                keyColumn: "catName",
+                keyValue: "Gaming");
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategory",
+                keyColumn: "catName",
+                keyValue: "Leisure");
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategory",
+                keyColumn: "catName",
+                keyValue: "Meeting");
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategory",
+                keyColumn: "catName",
+                keyValue: "Research");
+
+            migrationBuilder.DeleteData(
+                table: "ActivityCategory",
+                keyColumn: "catName",
+                keyValue: "Work");
+
+            migrationBuilder.DropColumn(
+                name: "categoryName",
+                table: "Activities");
+        }
+    }
+}
--- ActivityMonitorAPI/Model.cs
+++ ActivityMonitorAPI/Model.cs
@@ -27,6 +27,12 @@ public class ActivityDBContext : DbContext
             entity.HasKey(p => p.id);
             entity.Property(p => p.nameActivity);
             entity.Property(p => p.activityDescription);
+            entity.Property(p => p.categoryName);
+            
+            entity.HasOne(e => e.activityCategory)
+                .WithOne(e => e.activity)
+                .HasForeignKey<Activity>(a => a.categoryName)
+                .HasConstraintName("FK_activityCategory");
         });
         modelBuilder.Entity<ActivityDuration>(entity =>
         {
@@ -34,6 +40,11 @@ public class ActivityDBContext : DbContext
             entity.Property(p => p.activityId);
             entity.Property(p => p.durationTime);
             entity.Property(p => p.startTime);
+            
+            entity.HasOne(p => p.activity)
+                .WithOne(e => e.activityDuration)
+                .HasForeignKey<ActivityDuration>(p => p.activityId)
+                .HasConstraintName("FK_ActivityDuration_Activity");
         });
         
         modelBuilder.Entity<ActivityCategory>(entity =>
@@ -45,6 +56,160 @@ public class ActivityDBContext : DbContext
             entity.HasKey(p => p.id);
             entity.Property(p => p.userName);
             entity.Property(p => p.email);
+        });
+
+        modelBuilder.Entity<ActivityCategory>().HasData(new List<ActivityCategory>()
+        {
+            new ActivityCategory()
+            {
+                catName = "Work"
+            },
+            new ActivityCategory()
+            {
+                catName = "Leisure"
+            },
+            new ActivityCategory()
+            {
+                catName = "Research"
+            },
+            new ActivityCategory()
+            {
+                catName = "Afk"
+            },
+            new ActivityCategory()
+            {
+                catName = "Meeting"
+            },
+            new ActivityCategory()
+            {
+                catName = "Gaming"
+            }
+        });
+
+        modelBuilder.Entity<Activity>().HasData(new List<Activity>()
+        {
+            new Activity()
+            {   
+                id = 1,
+                nameActivity = "Coding",
+                activityDescription = "Actively using IDE",
+                categoryName = "Work",
+            },
+            new Activity()
+            {   
+                id = 2,
+                nameActivity = "Watching youtube",
+                activityDescription = "Relaxing on youtube",
+                categoryName = "Leisure"
+            },
+            new Activity()
+            {   
+                id = 3,
+                nameActivity = "Stackoverflow",
+                activityDescription = "Acquiring new knowledge",
+                categoryName = "Research"
+            },
+            new Activity()
+            {   
+                id = 4,
+                nameActivity = "No user activity",
+                activityDescription = "PC is idle",
+                categoryName = "Afk"
+            },
+            new Activity()
+            {   
+                id = 5,
+                nameActivity = "Discord Meeting",
+                activityDescription = "Planning..? Shittalking..? Its on discord at least.",
+                categoryName = "Meeting"
+            },
+            new Activity()
+            {   
+                id = 6,
+                nameActivity = "Playing OSRS",
+                activityDescription = "Limiting EXP waste",
+                categoryName = "Gaming"
+            },
+        });
+
+        modelBuilder.Entity<ActivityDuration>().HasData(new List<ActivityDuration>()
+        {
+            new ActivityDuration()
+            {
+                id = 1,
+                activityId = 1,
+                startTime = DateTime.Now,
+                durationTime = TimeSpan.FromHours(2)
+            },
+            new ActivityDuration()
+            {
+                id = 2,
+                activityId = 2,
+                startTime = DateTime.Now,
+                durationTime = TimeSpan.FromHours(2)
+            },
+            new ActivityDuration()
+            {
+                id = 3,
+                activityId = 3,
+                startTime = DateTime.Now,
+                durationTime = TimeSpan.FromHours(2)
+            },
+            new ActivityDuration()
+            {
+                id = 4,
+                activityId = 4,
+                startTime = DateTime.Now,
+                durationTime = TimeSpan.FromHours(2)
+            },
+            new ActivityDuration()
+            {
+                id = 5,
+                activityId = 5,
+                startTime = DateTime.Now,
+                durationTime = TimeSpan.FromHours(2)
+            },
+            new ActivityDuration()
+            {
+                id = 6,
+                activityId = 6,
+                startTime = DateTime.Now,
+                durationTime = TimeSpan.FromHours(2)
+            },
+        });
+
+        modelBuilder.Entity<Users>().HasData(new List<Users>()
+        {
+            new Users()
+            {
+                id = 1,
+                userName  = "Lars Henning",
+                email = "larsGmanHenning@gmail.com",
+            },
+            new Users()
+            {
+                id = 2,
+                userName  = "Peter Paludan",
+                email = "Paludan@gmail.com",
+            },
+            new Users()
+            {
+                id = 3,
+                userName  = "Henriette",
+                email = "henribobendi@gmail.com",
+            },
+            new Users()
+            {
+                id = 4,
+                userName  = "Jens Jensen",
+                email = "JensJensJens@gmail.com",
+            },
+            new Users()
+            {
+                id = 5,
+                userName  = "Bent Belastende",
+                email = "belastende@gmail.com",
+            },
         });
     }
 }
@@ -62,14 +227,23 @@ public partial class Activity
     [Required]
     [Column(TypeName = "varchar(200)")]
     public string activityDescription{ get; set; }
-    }
+    
+    [Required] 
+    [Column(TypeName = "varchar(100)")] 
+    public string categoryName { get; set; }
+    
+    public virtual ActivityDuration activityDuration { get; set; } 
+    public virtual ActivityCategory activityCategory { get; set; }
+}
 
 public partial class ActivityCategory
 {
     [Required]
     [Column(TypeName = "varchar(100)")]
     public string catName{ get; set; }
-    }
+    
+    public virtual Activity activity{ get; set; }
+}
 
 public partial class ActivityDuration
 {
@@ -85,6 +259,8 @@ public partial class ActivityDuration
     public DateTime startTime{ get; set; }
     
     public TimeSpan durationTime { get; set; }
+    
+    public Activity activity{ get; set; }
 }
 
 public partial class Users
